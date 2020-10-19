2adbf2f300c8823520e0bb702648448831c3bf3f
naina <nanu02d8@gmail.com>
2020-10-19 20:24:39 +0530

    Added response to get request

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req,res) => {
+    // console.log(request);
+    res.send("<h1>Hello world</h1>");
+})
+
 app.listen(3000, function(){
     console.log("Server has started on port 3000");
 });
