97070490b3465500ca6c6e46d3972f5ddc18121d
naina <nanu02d8@gmail.com>
2020-10-19 20:54:37 +0530

    Added contact and about webpages

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req,res) => {
-    // console.log(request);
     res.send("<h1>Hello world</h1>");
 })
+app.get("/contact", (req,res) => {
+    res.send("contact me @ nanu02d8@gmail.com");
+})
+
+app.get("/about", (req,res) => {
+    res.send("Hey there! This is Naina Upadhyay and I am currently learning Express . Thank you everyone, Have a nice day :-* :-)");
+})
+
+
 
 app.listen(3000, function(){
     console.log("Server has started on port 3000");
